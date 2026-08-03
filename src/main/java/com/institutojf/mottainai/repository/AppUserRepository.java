eb097dc34860b38f23da956edb27005371cd0076
keKvnJun <7377kevin@gmail.com>
2026-08-03 11:28:27 -0300

    feat: add AppUserRepository for user data access with custom query method

---
+++ src/main/java/com/institutojf/mottainai/repository/AppUserRepository.java
@@ -0,0 +1,11 @@
+package com.institutojf.mottainai.repository;
+
+import com.institutojf.mottainai.model.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AppUserRepository extends JpaRepository<AppUser, Integer> {
+
+    Optional<AppUser> findByEmailIgnoreCaseAndActiveTrueAndDeletedAtIsNull(String email);
+}
