b4a4a89dbe61a17bd9662bc986ef2a400dd4b824
keKvnJun <7377kevin@gmail.com>
2026-08-02 21:14:24 -0300

    chore: add swagger openapi configuration

---
+++ src/main/java/com/institutojf/mottainai/config/OpenApiConfig.java
@@ -0,0 +1,29 @@
+package com.institutojf.mottainai.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI mottainaiOpenApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Mottainai API")
+                        .version("v1")
+                        .description("REST API for predictive retail inventory management")
+                        .license(new License().name("Academic project")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components().addSecuritySchemes("bearerAuth", new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")));
+    }
+}
